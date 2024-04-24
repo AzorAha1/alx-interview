05ae5d1b32c18e4aa0ba44fb63dbe48346bb8e9e
AzorAha1 <wizfaiz@icloud.com>
2024-04-24 21:47:25 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,8 +10,8 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
+    operations = 0
     for i in range(2, n+1):
-        operations = 0
         while(n % i == 0):
             n //= i
             operations += i
