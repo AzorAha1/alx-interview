1314fe8e70fc89039f073a0a7dbf395a3cb091c0
AzorAha1 <wizfaiz@icloud.com>
2024-04-24 21:51:23 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,10 +11,14 @@ def minOperations(n):
     if n <= 1:
         return 0
     operations = 0
+    primefactorsofn = []
     for i in range(2, n+1):
         while(n % i == 0):
             n //= i
-            operations += i
+            primefactorsofn.append(i)
             if (n == 1):
                 break
+    for primefactor in primefactorsofn:
+        if primefactor != 1:
+            operations += primefactor
     return operations
