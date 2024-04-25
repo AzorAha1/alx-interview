b46ce3b08f2882e4b05dd44f7a14601298c46549
AzorAha1 <wizfaiz@icloud.com>
2024-04-25 15:09:34 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,12 +6,13 @@ def minOperations(n):
     """function to get min operations"""
     if n <= 1:
         return 0
+
     primefactorsofn = []
     for i in range(2, n + 1):
         while n % i == 0:
             n //= i
             primefactorsofn.append(i)
-            if n == 1:
-                break
+        if n == 1:
+            break
     operations = sum(primefactorsofn)
     return operations
