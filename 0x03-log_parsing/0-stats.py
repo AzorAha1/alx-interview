4a55566cf24c7d6aee29bc2cfd300db9c32a5108
AzorAha1 <wizfaiz@icloud.com>
2024-05-02 23:54:11 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,23 +20,25 @@ def signal_handler(sig, frame):
 
 possible_statuscode = [200, 301, 400, 403, 404, 405, 500]
 signal.signal(signal.SIGINT, signal_handler)
-linepattern = re.compile(
-    r'^\S+ - \[\d{4}-\d{2}-\d{2}\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$'
-    )
+linep = re.compile(
+    r'^\S+ - \[\d{4}-\d{2}-\d{2}\] '     # Match IP and date
+    r'"GET \/projects\/\d+ HTTP\/1\.1" '  # Match request line
+    r'(\d{3}) '                           # Match status code
+    r'(\d+)$'                             # Match file size
+)
 line_counter = 0
 total_size = 0
 count_status_code = {code: 0 for code in possible_statuscode}
 while(True):
     line = sys.stdin.readline().strip()
-    
-    if linepattern.match(line):
-        file_size = linepattern.search(line).group(2)
-        status_code = int(linepattern.search(line).group(1))
+    if linep.match(line):
+        file_size = linep.search(line).group(2)
+        status_code = int(linep.search(line).group(1))
         total_size = total_size + int(file_size)
         count_status_code[status_code] += 1
         line_counter += 1
         print('line inputed')
-        if line_counter == 3 :
+        if line_counter == 3:
             for code, count in count_status_code.items():
                 print(f'{code}: {count}')
             print(f'File size: {total_size}')
