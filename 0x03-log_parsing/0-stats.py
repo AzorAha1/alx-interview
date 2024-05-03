dfb39c73ee4f4e392079b23b08eb0a63bf8a3bf2
AzorAha1 <wizfaiz@icloud.com>
2024-05-03 01:14:52 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,20 +10,20 @@ import sys
 import re
 import signal
 
-linep = linep = re.compile(r'^\S+ - \[(\d{4}-\d{2}-\d{2})\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$')
+linep = re.compile(r'^\S+ - \[(\d{4}-\d{2}-\d{2})\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$')
 def signal_handler(sig, frame):
     """this is the summary"""
-    print(f'File size: {total_size}')
-    for code in sorted(count_status_code.keys()):
-        if code in possible_statuscode:
-            print(f'{code}: {count_status_code[code]}')
+    print_stats()
     sys.exit(0)
 
-
+def print_stats():
+    """print stats"""
+    print(f'File size: {total_size}')
+    for code in sorted(count_status_code.keys()):
+        if count_status_code[code] > 0:
+            print(f'{code}: {count_status_code[code]}')
 possible_statuscode = [200, 301, 400, 403, 404, 405, 500]
 signal.signal(signal.SIGINT, signal_handler)
-# linep = linep = re.compile(r'^\S+ - \[(\d{4}-\d{2}-\d{2})\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$')
-
 line_counter = 0
 total_size = 0
 count_status_code = {code: 0 for code in possible_statuscode}
@@ -33,14 +33,14 @@ try:
         if linep.match(line):
             status_code = int(linep.search(line).group(1))
             file_size = int(linep.search(line).group(2))
-            total_size = total_size + int(file_size)
-            count_status_code[status_code] += 1
+            total_size = total_size + file_size
+            if status_code in count_status_code:
+                count_status_code[status_code] += 1
             line_counter += 1
             if line_counter == 10:
-                print(f'File size: {total_size}')
-                for code in sorted(count_status_code.keys()):
-                    if code in possible_statuscode:
-                        print(f'{code}: {count_status_code[code]}')
+                print_stats()
                 line_counter = 0
 except KeyboardInterrupt:
     signal_handler(signal.SIGINT, None)
+finally:
+    print_stats()
