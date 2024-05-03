e1977024d2b93fb000143aacb73c67e97b6a1db6
AzorAha1 <wizfaiz@icloud.com>
2024-05-03 02:00:26 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,42 +7,35 @@ Return: return_description
 """
 
 import sys
-import re
-import signal
 
-linep = re.compile(r'^\S+ - \[(\d{4}-\d{2}-\d{2})\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$')
-def signal_handler(sig, frame):
-    """this is the summary"""
-    print_stats()
-    sys.exit(0)
-
-def print_stats():
-    """print stats"""
-    print(f'File size: {total_size}')
-    for code in sorted(possible_statuscode):
-        if code in count_status_code and count_status_code[code] > 0:
-            print(f'{code}: {count_status_code[code]}')
-possible_statuscode = [200, 301, 400, 403, 404, 405, 500]
-signal.signal(signal.SIGINT, signal_handler)
+statuscode_dict = {"200": 0, "301": 0, "400": 0, "403": 0,
+                   "404": 0, "405": 0, "500": 0}
 line_counter = 0
 total_size = 0
-count_status_code = {code: 0 for code in possible_statuscode}
+
 try:
     for line in sys.stdin:
         line = line.strip()
-        if linep.match(line):
-            status_code = int(linep.search(line).group(2))
-            file_size = int(linep.search(line).group(3))
-            total_size = total_size + file_size
-            if status_code in count_status_code:
-                count_status_code[status_code] += 1
+        lines = line.split(" ")
+        if len(lines) > 4:
+            status_code = lines[-2]
+            file_size = int(lines[-1])
+            if status_code in statuscode_dict:
+                statuscode_dict[status_code] += 1
+            total_size += file_size
             line_counter += 1
             if line_counter == 10:
-                print_stats()
                 line_counter = 0
-        else:
-            continue
+                print(f'File size: {total_size}')
+                for code in sorted(statuscode_dict):
+                    if statuscode_dict[code] != 0:
+                        print(f'{code}: {statuscode_dict[code]}')
+
 except KeyboardInterrupt:
-    signal_handler(signal.SIGINT, None)
+    pass
+
 finally:
-    print_stats()
+    print(f'File size: {total_size}')
+    for code in sorted(statuscode_dict):
+        if statuscode_dict[code] != 0:
+            print(f'{code}: {statuscode_dict[code]}')
