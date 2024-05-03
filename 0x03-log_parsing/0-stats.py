30fecf1a70f66569514a575575aa8050477ac460
AzorAha1 <wizfaiz@icloud.com>
2024-05-03 01:08:48 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,12 +22,8 @@ def signal_handler(sig, frame):
 
 possible_statuscode = [200, 301, 400, 403, 404, 405, 500]
 signal.signal(signal.SIGINT, signal_handler)
-linep = re.compile(
-    r'^\S+ - \[\d{4}-\d{2}-\d{2}\] '
-    r'"GET \/projects\/\d+ HTTP\/1\.1" '
-    r'(\d{3}) '
-    r'(\d+)$'
-    )
+linep = linep = re.compile(r'^\S+ - \[(\d{4}-\d{2}-\d{2})\] "GET \/projects\/\d+ HTTP\/1\.1" (\d{3}) (\d+)$')
+
 line_counter = 0
 total_size = 0
 count_status_code = {code: 0 for code in possible_statuscode}
