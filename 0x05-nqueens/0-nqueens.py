fbd2eb0ee584b5b5ac8246872537845d64ef3e8c
AzorAha1 <wizfaiz@icloud.com>
2024-05-16 14:46:36 +0100

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""N QUEENS"""
+
+
 import sys
 
+
 def parsearguments():
+    """parse argument"""
     if len(sys.argv) != 2:
         print('Usage: nqueens N')
         sys.exit(1)
@@ -10,12 +15,14 @@ def parsearguments():
         if n < 4:
             print('N must be at least 4')
             sys.exit(1)
+        return n
     except ValueError:
         print('N must be a number')
         sys.exit(1)
-    print(n)
-    
+
+
 def is_safe(board, row, col):
+    """is safe to check if its ok to insert queen"""
     for i in range(row):
         if board[i] == col:
             return False
@@ -24,8 +31,30 @@ def is_safe(board, row, col):
     return True
 
 
+def solve_nqueen(n):
+    def backtrackalgo(row, board, solutions):
+        """backtracking algorithm"""
+        if row == n:
+            solutions.append(board[:])
+        for col in range(n):
+            if is_safe(board=board, row=row, col=col):
+                board[row] = col
+                backtrackalgo(board=board, row=row + 1, solutions=solutions)
+                board[row] = -1
+    solutions = []
+    board = [-1] * n
+    backtrackalgo(row=0, board=board, solutions=solutions)
+    return solutions
+
+
 def main():
-    parsearguments()
+    """main function"""
+    n = parsearguments()
+    solutions = solve_nqueen(n)
+    for solution in solutions:
+        tailoredsolution = [[r, c] for r, c in enumerate(solution)]
+        print(tailoredsolution)
+
 
 if __name__ == '__main__':
     main()
