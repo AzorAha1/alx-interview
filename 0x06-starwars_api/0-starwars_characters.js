0d9ac51434c68da8d579e1f32cca9096fbd4c681
AzorAha1 <wizfaiz@icloud.com>
2024-05-21 19:39:51 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,21 +4,23 @@ const request = require('request');
 const myarg = process.argv[2];
 const thefilms = `https://swapi-api.alx-tools.com/api/films/${myarg}`;
 
-request.get(thefilms, (error, response, body) => {
+request.get(thefilms, async (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
   const filmdata = JSON.parse(body);
   const CharacterData = filmdata.characters;
-  CharacterData.forEach(theurls => {
-    request.get(theurls, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-      const thecharacters = JSON.parse(body);
-      console.log(thecharacters.name);
+  for (const url of CharacterData) {
+    await new Promise((resolve, reject) => {
+      request.get(url, (error, response, body) => {
+        if (error) {
+          resolve(error);
+        }
+        const eachcharacter = JSON.parse(body);
+        console.log(eachcharacter.name);
+        resolve();
+      });
     });
-  });
+  }
 });
