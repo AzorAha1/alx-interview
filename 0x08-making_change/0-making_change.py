10e7ec6630f27725a4dfa6d9c801d710f5a30c41
AzorAha1 <wizfaiz@icloud.com>
2024-06-06 10:06:56 +0000

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,12 +5,11 @@
 def makeChange(coins, total):
     """make changes"""
     if total < 1:
-        return 0;
-    thelistformin = [float('inf')] * (total + 1);
-    thelistformin[0] = 0;
+        return 0
+    listformin = [float('inf')] * (total + 1)
+    listformin[0] = 0
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
-                thelistformin[i] = min(thelistformin[i], 1 + thelistformin[i - coin]);
-    return -1 if thelistformin[total] == float('inf') else thelistformin[total]
-
+                listformin[i] = min(listformin[i], 1 + listformin[i - coin])
+    return -1 if listformin[total] == float('inf') else listformin[total]
