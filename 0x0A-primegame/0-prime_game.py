4dbe8cce173ea66411fa771e85e9386a1b94752d
faiz <wizfaiz@icloud.com>
2024-06-20 19:40:47 -0700

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,39 +13,37 @@ def is_prime(n):
 
 
 def isWinner(x, nums):
-    """Determine the winner of each round."""
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_nums = [num for num in range(2, n + 1) if is_prime(num)]
-        current_player = 'Maria'
-        remaining_nums = list(range(1, n + 1))
-
-        while True:
-            player = current_player
-            chosen_prime = None
-
-            for prime in prime_nums:
-                if prime in remaining_nums:
-                    chosen_prime = prime
-                    break
-
-            if not chosen_prime:
-                if player == 'Maria':
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            remaining_nums = [
-                num for num in remaining_nums if num % chosen_prime != 0
-                ]
-            current_player = 'Maria' if current_player == 'Ben' else 'Ben'
-
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    else:
+    """Determine the winner of each round using dynamic programming."""
+    if not nums or x < 1:
         return None
+
+    # Find the maximum number in nums
+    max_num = max(nums)
+
+    # Implement the Sieve of Eratosthenes to find all primes up to max_num
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    # Count the number of primes up to each index
+    prime_count = [0] * (max_num + 1)
+    for i in range(2, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + is_prime[i]
+
+    player_1 = 0
+    for p in nums:
+        # Maria wins if the count of primes up to p is odd
+        player_1 += prime_count[p] % 2 == 1
+
+    # Determine the overall winner
+    if player_1 * 2 == len(nums):
+        return None
+
+    if player_1 * 2 > len(nums):
+        return "Maria"  # Maria wins more rounds than Ben
+
+    return "Ben"  # Ben wins more rounds than Maria
